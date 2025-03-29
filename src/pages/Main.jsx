7d38bcf22dfc95a815c4d4fa1de317416b10707d
phonkiatrobkob05 <phonkiatrobkob05@gmail.com>
2025-03-29 17:00:46 +0700

    setting up

---
+++ src/pages/Main.jsx
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <div className="main-page">
+      <h1>Welcome to the Main Page</h1>
+      <p>This is the main content area.</p>
+    </div>
+  );
+}
+export default Main;
